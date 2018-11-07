42e2dcaf1dbc3d932ac41edbffcdf5ba81e111f8
Alexander Borodin <a.borodin95@gmail.com>
2018-11-07 22:38:05 +0300

    Initial commit.

---
+++ src/main/java/CallbackApiHandler.java
@@ -0,0 +1,9 @@
+import com.vk.api.sdk.callback.CallbackApi;
+import com.vk.api.sdk.objects.messages.Message;
+
+public class CallbackApiHandler extends CallbackApi {
+    @Override
+    public void messageNew(Integer groupId, Message message) {
+        System.out.println(message.getBody());
+    }
+}
